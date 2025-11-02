c7d0cb17dd86a5e26c4ce09463f585f99f5d46eb
Ishidawg <willianscagol@gmail.com>
2025-11-02 15:21:38 -0300

    add d3d9, d3d10 and open opengl some game like farcry 'the first one' runs with dxvk but under the hood reads d3d9 instructions

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def user_input():
   git_clone_effects()
 
   while True:
-    game_bits = int((input("Your game is 32bit or 64bit? [1] - [2]: ")))
+    game_bits = abs(int((input("Your game is 32bit or 64bit? [1] - [2]: "))))
 
     if game_bits <= 2:
       break
@@ -33,41 +33,66 @@ def user_input():
     local_source = find_reshade('./reshade', 'ReShade32.dll')
   else:
     local_source = find_reshade('./reshade', 'ReShade64.dll')
-  
-  # print("local_source: " + local_source)
 
   while True:
-    game_api = int(input("Is your game api Vulkan or openGL? [1] - [2]: "))
+    print("What is your game API?")
+    print("[1] - Vulkan")
+    print("[2] - OpenGL")
+    print("[3] - d3d9")
+    print("[4] - d3d10")
 
-    if game_api <= 2:
+    game_api = abs(int(input("Choice: ")))
+
+    if game_api <= 4:
       break
 
-  if game_api == 1:
-    # print("Cooking a Vulkan dll...")
-    new_dll = ready_dll(local_source, 'dxgi.dll',)
-    correct_dll = find_reshade('./reshade', 'dxgi.dll')
-  else:
-    # print("Cooking a openGL dll...")
-    new_dll = ready_dll(local_source, 'opengl.dll')
-    correct_dll = find_reshade('./reshade', 'opengl.dll')
+  # Match "switch case"
+  match game_api:
+    case 1:
+      new_dll = ready_dll(local_source, 'dxgi.dll',)
+      correct_dll = find_reshade('./reshade', 'dxgi.dll')
+    case 2:
+      new_dll = ready_dll(local_source, 'opengl.dll')
+      correct_dll = find_reshade('./reshade', 'opengl.dll')
+    case 3:
+      new_dll = ready_dll(local_source, 'd3d9.dll',)
+      correct_dll = find_reshade('./reshade', 'd3d9.dll')
+    case 4:
+      new_dll = ready_dll(local_source, 'd3d10.dll',)
+      correct_dll = find_reshade('./reshade', 'd3d10.dll')
 
-  # print(correct_dll)
+  # Old if statement
+  # if game_api == 1:
+  #   new_dll = ready_dll(local_source, 'dxgi.dll',)
+  #   correct_dll = find_reshade('./reshade', 'dxgi.dll')
+  # else:
+  #   new_dll = ready_dll(local_source, 'opengl.dll')
+  #   correct_dll = find_reshade('./reshade', 'opengl.dll')
 
   game_source = str(input("What is your game directory: "))
-  # print(game_source)
 
   # copy reshade files to games folder
-  shutil.copyfile(correct_dll, f'{game_source}/{new_dll}')
-  shutil.copytree('reshade/effects/Shaders', f'{game_source}/Shaders', dirs_exist_ok=True)
-  shutil.copytree('reshade/effects/Textures', f'{game_source}/Textures', dirs_exist_ok=True)
+  copy_reshade_to_games_folder(correct_dll, game_source, new_dll)
 
 def ready_dll(local, new_name):
   os.system(f'cp {local} reshade/{new_name}')
   return new_name
 
-def git_clone_effects():
-  os.system("git clone https://github.com/crosire/reshade-shaders.git ./reshade/effects")
+def copy_reshade_to_games_folder(correct_dll, game_source, new_dll):
+  shutil.copyfile(correct_dll, f'{game_source}/{new_dll}')
 
-# print("Path:", find_reshade('/home', 'ReShade_Setup*.exe'))
-# unzip_reshade(find_reshade('/home', 'ReShade_Setup*.exe'))
+  # if len(os.listdir('./reshade/effects')) == 0:
+  #   os.system("git clone https://github.com/crosire/reshade-shaders.git ./reshade/effects")
+  # else:    
+  #   print("We already have shaders downloaded.")
+
+  shutil.copytree('./reshade/effects/Shaders', f'{game_source}/Shaders', dirs_exist_ok=True)
+  shutil.copytree('./reshade/effects/Textures', f'{game_source}/Textures', dirs_exist_ok=True)
+
+def git_clone_effects():
+  if len(os.listdir('./reshade/effects')) == 0:
+    os.system("git clone https://github.com/crosire/reshade-shaders.git ./reshade/effects")
+  else:    
+    print("We already have shaders downloaded.")
+
 user_input()
