c490607189e36d4a3a6ce050f5e6653d67c9ff2b
Ishidawg <willianscagol@gmail.com>
2026-02-02 20:03:13 -0300

    add installation and enhance general code

--- main.py
+++ main.py
@@ -42,10 +42,13 @@ class MainWindow(QMainWindow):
         self.page_download: PageDownload = PageDownload()
         self.page_installation: PageInstallation = PageInstallation()
 
-        self.pages: list[QWidget] = [
-            self.page_download, self.page_installation]
+        self.pages: list[QWidget] = [self.page_start,
+                                     self.page_download, self.page_installation]
         self.pages_index: int = 0
         self.current_page: QWidget = self.pages[0]
+
+        self.is_home: bool = False
+        self.is_installation: bool = False
 
         print(f"len: {len(self.pages)}")
         print(f"idx: {self.pages_index}")
@@ -66,21 +69,31 @@ class MainWindow(QMainWindow):
         self.layout_main.addWidget(widget_dinamic)
         self.layout_main.addWidget(self.action_buttons)
 
-    def on_home_clicked(self):
+    def on_home_clicked(self) -> None:
+        self.pages_index = 0
+        self.update_buttons()
         # TODO: need to create a function
+        self.is_home = True
         self.layout_dynamic.removeWidget(self.current_page)
         self.layout_dynamic.addWidget(self.page_start)
-        self.action_buttons.btn_home.hide()
-        self.action_buttons.btn_next.hide()
-        self.action_buttons.btn_back.hide()
 
-    def on_back_clicked(self):
+    def on_back_clicked(self) -> None:
         self.change_page(0)
 
-    def on_next_clicked(self):
+    def on_next_clicked(self) -> None:
         self.change_page(1)
 
-    def change_page(self, direction: int = 1):
+    def update_buttons(self) -> None:
+        if self.pages_index == 0:
+            self.action_buttons.btn_home.hide()
+            self.action_buttons.btn_next.hide()
+            self.action_buttons.btn_back.hide()
+        elif self.pages_index == 1:
+            self.action_buttons.btn_home.show()
+            self.action_buttons.btn_next.show()
+            self.action_buttons.btn_back.show()
+
+    def change_page(self, direction: int = 1) -> None:
         self.layout_dynamic.removeWidget(self.current_page)
 
         print(self.current_page)
@@ -89,19 +102,15 @@ class MainWindow(QMainWindow):
                 if self.pages_index > 0:
                     self.pages_index -= 1
                     print("-")
-                    print(f"len: {len(self.pages)}")
-                    print(f"idx: {self.pages_index}")
-
             case 1:
                 if self.pages_index < len(self.pages) - 1:
                     self.pages_index += 1
-                print("+")
-                print(f"len: {len(self.pages)}")
-                print(f"idx: {self.pages_index}")
+                    print("+")
             case _:
                 print("Error trying to change pages")
 
         self.current_page = self.pages[self.pages_index]
+        self.update_buttons()
         print(self.current_page)
 
         # TODO: implement a method to do this, could call insert_page
@@ -114,14 +123,17 @@ class MainWindow(QMainWindow):
     @Slot(bool)
     def on_install_clicked(self, value: bool) -> None:
         if value:
+            self.change_page(1)
+            # self.update_buttons()
             # TODO: implement a method to do this
-            self.pages_index = 1
-            self.current_page = self.pages[0]
-            self.layout_dynamic.removeWidget(self.page_start)
-            self.layout_dynamic.addWidget(self.current_page)
-            self.action_buttons.btn_home.show()
-            self.action_buttons.btn_back.show()
-            self.action_buttons.btn_next.show()
+            # self.is_installation = True
+            # self.pages_index = 1
+            # self.current_page = self.pages[0]
+            # self.layout_dynamic.removeWidget(self.page_start)
+            # self.layout_dynamic.addWidget(self.current_page)
+            # self.action_buttons.btn_home.show()
+            # self.action_buttons.btn_back.show()
+            # self.action_buttons.btn_next.show()
 
     @Slot(bool)
     def on_uninstall_clicked(self, value: bool) -> None:
--- widgets/pages/page_installation.py
+++ widgets/pages/page_installation.py
@@ -1,4 +1,7 @@
 from PySide6.QtWidgets import (
+    QGridLayout,
+    QLineEdit,
+    QRadioButton,
     QWidget,
     QVBoxLayout,
     QHBoxLayout,
@@ -20,26 +23,50 @@ class PageInstallation(QWidget):
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        layout_buttons = QHBoxLayout()
+        layout_browse = QHBoxLayout()
+        layout_api = QGridLayout()
+        layout_api.setSpacing(10)
 
         # create widgets
-        label_description = QLabel(
-            "I like to do backflips...")
-        label_description.setStyleSheet("font-size: 12pt; font-weight: 100")
-        label_description.setWordWrap(True)
+        label_exe = QLabel("Select game executable")
+        label_exe.setStyleSheet("font-size: 12pt; font-weight: 100")
+        label_exe.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.btn_install = QPushButton("Install")
-        self.btn_uninstall = QPushButton("Uninstall")
+        self.browse_input = QLineEdit()
+        self.browse_button = QPushButton("browse")
 
-        self.btn_install.clicked.connect(self.click_install)
-        self.btn_uninstall.clicked.connect(self.click_uninstall)
+        label_api = QLabel("Select game api")
+        label_api.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        label_api.setStyleSheet("font-size: 12pt; font-weight: 100")
+
+        self.radio_opengl = QRadioButton("OpenGL")
+        self.radio_d3d8 = QRadioButton("D3D 8")
+        self.radio_d3d9 = QRadioButton("D3D 9")
+        self.radio_d3d10 = QRadioButton("D3D 10")
+        self.radio_d3d11 = QRadioButton("D3D 11")
+        self.radio_vulkan = QRadioButton("Vulkan/D3D 12")
+        self.radio_vulkan.setChecked(True)
 
         # add widgets
-        layout.addWidget(label_description)
+        layout.addWidget(label_exe)
 
-        layout_buttons.addWidget(self.btn_install)
-        layout_buttons.addWidget(self.btn_uninstall)
-        layout.addLayout(layout_buttons)
+        layout_browse.addWidget(self.browse_input)
+        layout_browse.addWidget(self.browse_button)
+        layout.addLayout(layout_browse)
+
+        layout_api.addWidget(self.radio_opengl, 0, 0)
+        layout_api.addWidget(self.radio_d3d8, 0, 1)
+        layout_api.addWidget(self.radio_d3d9, 0, 2)
+        layout_api.addWidget(self.radio_d3d10, 1, 0)
+        layout_api.addWidget(self.radio_d3d11, 1, 1)
+        layout_api.addWidget(self.radio_vulkan, 1, 2)
+        layout.addLayout(layout_api)
+
+        # self.btn_install = QPushButton("Install")
+        # self.btn_uninstall = QPushButton("Uninstall")
+
+        # self.btn_install.clicked.connect(self.click_install)
+        # self.btn_uninstall.clicked.connect(self.click_uninstall)
 
         self.setLayout(layout)
 
